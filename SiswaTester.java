2fff674669aacc705f9afcd5f8ba1bfac1d489c4
Pashatania Fitri Indah Lestari <87978506+pashatania1@users.noreply.github.com>
2024-08-22 09:37:57 +0700

    Add files via upload

--- SiswaTester.java
+++ SiswaTester.java
@@ -1,20 +1,31 @@
+
 //Driver Class
+import java.util.Scanner;
+
 public class SiswaTester {
     public static void main(String[] args) {
-       //object
-       Siswa lia = new Siswa(); 
-       Siswa andi = new Siswa();
-       //object dgn constructor parameter
-       Siswa via = new Siswa(890, "Via Vallen", 70.5);
-       
-       //mengubah nilai variabel
-       lia.id = 12345;
-       lia.nama = "Aprilia";
-       lia.ipk = 90;
-       System.out.println(lia.id);
-       System.out.println(lia.nama);
-       System.out.println(lia.ipk);
-       
-       System.out.println(andi.nama);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Inputkan nama lengkap: ");
+        String nama = in.nextLine();
+        System.out.println("Inputkan ID Siswa: ");
+        int id = in.nextInt();
+        System.out.println("Inputkan IPK Siswa :");
+        double ipk = in.nextDouble();
+        // object
+        Siswa lia = new Siswa();
+        Siswa andi = new Siswa();
+        // object dgn constructor parameter
+        Siswa via = new Siswa(890, "Via Vallen", 70.5);
+        Siswa dana = new Siswa(id, nama, ipk);
+
+        // mengubah nilai variabel
+        lia.id = 12345;
+        lia.nama = "Aprilia";
+        lia.ipk = 90;
+        System.out.println(lia.id);
+        System.out.println(lia.nama);
+        System.out.println(lia.ipk);
+
+        System.out.println(andi.nama);
     }
 }
